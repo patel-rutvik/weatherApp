9946cc6fef08bdd5704ceadf29e722209d8c1ea9
a-majeed <45409413+a-majeed@users.noreply.github.com>
2020-01-18 16:05:48 -0700

    Add files via upload
    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,20 @@
 function getWeather() {    
     let getCity = $('#cityInput').val();
+    let metric = "&units=metric";
     let apiID = '&appid=86e5c8fb817d787866c82bf6406b4923';
-    let apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + getCity + apiID;
+    let apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + getCity + metric + apiID;
     
     $.getJSON(apiCall, updateData);
 }
 
 function updateData(weatherData) {
     let icon = weatherData.weather[0].icon;
-    let min = Math.round(10*(weatherData.main.temp_min - 273.15))/10;
-    let max = Math.round(10*(weatherData.main.temp_max - 273.15))/10;
-    let temp = Math.round(10*(weatherData.main.temp - 273.15))/10;
+    let min = Math.round(10*(weatherData.main.temp_min ))/10;
+    let max = Math.round(10*(weatherData.main.temp_max ))/10;
+    let temp = Math.round(10*(weatherData.main.temp))/10;
     let sunrise = weatherData.sys.sunrise;
     let sunset = weatherData.sys.sunset;
-    let feels_like = weatherData.main.feels_like;
+    let feels_like = Math.round(10*(weatherData.main.feels_like))/10;
     let lat = weatherData.coord.lat;
     let lon = weatherData.coord.lon;
     let description = weatherData.weather[0].description;
@@ -21,6 +22,7 @@ function updateData(weatherData) {
     let direction = weatherData.wind.deg;
     let humidity = weatherData.main.humidity;
     let pressure = weatherData.main.pressure;
+    
 
     document.getElementById("weather").textContent = temp.toString() + " °C";
     document.getElementById("description").textContent = capitalizeFirstLetter(description);
@@ -34,6 +36,7 @@ function updateData(weatherData) {
     document.getElementById("sunset").textContent = formatTime(sunset) + " UTC";
     document.getElementById("longitude").textContent = lon;
     document.getElementById("latitude").textContent = lat;
+    document.getElementById("feelsLike").textContent = feels_like.toString() + "°C";
     let iconurl = 'http://api.openweathermap.org/img/w/' + icon + '.png';
     $('#weatherIcon').attr('src', iconurl);
 }
